d96a22eba282c4db66e260ccea94c779468aa20f
Katumba <jwagner212@googlemail.com>
2018-12-03 09:30:33 +0100

    Create Problem_1.R

---
+++ Problem_1.R
@@ -0,0 +1,29 @@
+
+# Assigment 1 -------------------------------------------------------------
+library(tidyverse)
+
+rm(list = ls())
+## 1st Problem
+## Simulate Probit Model with known Latent Variable
+
+# set seed for comparibility
+set.seed(101)
+# Xo ~ N(10, 2²)
+## set observation to 400
+xNorm1 <- rnorm(400,10,2) %>% round(2)
+
+# Error term
+errorTerm <- rnorm(400)
+
+# Latent Model for the joint multivariate Distribution
+yLatent <- -30 + 4 * xNorm1 + errorTerm
+## transform Latent Variable into Bernoulli Variable
+## y <- yLatent %>% 
+y <- yLatent %>% replace(yLatent<=0,0) %>% replace(yLatent>0,1)
+
+# yLatent[yLatent<=0] <- 0
+# yLatent[yLatent>0] <- 1
+
+# Probit Model
+probit<-glm(yLatent ~ xNorm1, family=binomial(link="probit"))
+probit$coefficients
